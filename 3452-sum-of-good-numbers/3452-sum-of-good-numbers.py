bb62dc9055985675a711a34fc8a2241a69d6c013
Subham Biswas <toysonray120@gmail.com>
2026-08-30 22:37:06 +0530

    Time: 2 ms (59.71%), Space: 19.3 MB (28.24%) - LeetHub

---
+++ 3452-sum-of-good-numbers/3452-sum-of-good-numbers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumOfGoodNumbers(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        ans = 0
+        
+        for i in range(n):
+            if i - k >= 0 and i + k < n and nums[i] > nums[i - k] and nums[i] > nums[i + k]:
+                ans += nums[i]
+            elif i - k < 0 and i + k < n and nums[i] > nums[i + k]:
+                ans += nums[i]
+            elif i - k >= 0 and i + k >= n and nums[i] > nums[i - k]:
+                ans += nums[i]
+            elif i - k < 0 and i + k >= n:
+                ans += nums[i]
+
+        return ans
